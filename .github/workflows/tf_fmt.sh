9a088c53a23adf8e745f3411fece9436d48ec78c
00inboxtest <00.inbox.test@gmail.com>
2022-01-03 12:29:01 +0530

    6 push

--- .github/workflows/tf_fmt.sh
+++ .github/workflows/tf_fmt.sh
@@ -1,8 +1,8 @@
-# exit if any of the commands 
+# exit if any of the commands fails
 set -o errexit
 
-# format checking of 
+# format checking of terraform
 terraform fmt -recursive -check -diff
 
-# validating 
+# validating configurations
 terraform validate
